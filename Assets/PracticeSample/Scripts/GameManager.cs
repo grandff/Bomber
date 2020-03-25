63a753667b23ce1e7a97fb72372b1c155d083d23
김정민 <dsaqaz421@naver.com>
2020-03-25 21:44:54 +0900

    맵 구현 완료
    GameManager와 Loader 구현

--- Assets/PracticeSample/Scripts/GameManager.cs
+++ Assets/PracticeSample/Scripts/GameManager.cs
@@ -2,8 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    // 싱글톤
+    public static GameManager instance = null;
+
+    public BoardManager boardScript;
+    private int level = 3;
+
+    void Awake() {
+        if (instance == null){
+            instance = this;
+        }else if(instance != this){
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);      // 씬이 넘어가도 점수 계산을 해야하기 때문
+        boardScript = GetComponent<BoardManager>();
+        InitGame();    
+    }
+
+    // init game
+    void InitGame(){
+        boardScript.SetupScene(level);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/PracticeSample/Scripts/Loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+
+    public GameObject gameManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // 인스턴스 초기화
+        if(GameManager.instance == null){
+            Instantiate(gameManager);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
