c58c2e0ffb2347615b0e98d04bbc00855fec202d
김정민 <dsaqaz421@naver.com>
2020-03-28 16:10:50 +0900

    벽 스크립트 생성
    파괴할 수 있는 벽 스크립트 생성

---
+++ Assets/PracticeSample/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    
+    public void DamageWall (int loss){
+        // 플레이어가 성공적으로 벽을 공격했을 떄 시각적인 변화를 줌
+        spriteRenderer.sprite = dmgSprite;
+        // 벽 파괴
+        hp -= loss;
+        if(hp <= 0) gameObject.SetActive(false);
+    }
+}
